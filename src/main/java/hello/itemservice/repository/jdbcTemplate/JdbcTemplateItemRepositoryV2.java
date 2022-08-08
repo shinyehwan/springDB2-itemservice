f8528685a80ba233c98cbe18ed29a2bdce27d344
shinyehwan <jghisjl007@naver.com>
2022-08-09 02:05:42 +0900

    JdbcTemplate - 이름 지정 파라미터1

--- src/main/java/hello/itemservice/repository/jdbcTemplate/JdbcTemplateItemRepositoryV2.java
+++ src/main/java/hello/itemservice/repository/jdbcTemplate/JdbcTemplateItemRepositoryV2.java
@@ -6,8 +6,13 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
@@ -17,35 +22,40 @@ import javax.sql.DataSource;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 
 /**
- * Jdbc Template
+ * NamedParameterJdbcTemplate
+ * SqlParameterSource
+ * - BeanPropertySqlParameterSource
+ * - MapSqlParameterSource
+ * Map
+ *
+ * BeanPropertyRowMapper
+ *
  */
 
 @Repository
 @Slf4j
-public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
+public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
 
-    private final JdbcTemplate template;
-    public JdbcTemplateItemRepositoryV1(DataSource dataSource) {
-        this.template = new JdbcTemplate(dataSource);
+    private final NamedParameterJdbcTemplate template;
+    public JdbcTemplateItemRepositoryV2(DataSource dataSource) {
+        this.template = new NamedParameterJdbcTemplate(dataSource);
     }
     @Override
     public Item save(Item item) {
-        String sql = "insert into item (item_name, price, quantity) values (?, ?, ?)";
+        String sql = "insert into item (item_name, price, quantity) " +
+                "values (:itemName, :price, :quantity)";
+
+        SqlParameterSource param = new BeanPropertySqlParameterSource(item);
+
         // DB에서 자동증가한 id 값을 select 해준다.
         KeyHolder keyHolder = new GeneratedKeyHolder();
         // 데이터 베이스에 인서트
-        template.update(connection -> {
-            //자동 증가 키
-            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
-            ps.setString(1, item.getItemName());
-            ps.setInt(2, item.getPrice());
-            ps.setInt(3, item.getQuantity());
-            return ps;
-        }, keyHolder);
+        template.update(sql, param, keyHolder);
 
         long key = keyHolder.getKey().longValue();
         item.setId(key);
@@ -54,20 +64,22 @@ public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
 
     @Override
     public void update(Long itemId, ItemUpdateDto updateParam) {
-        String sql = "update item set item_name=?, price=?, quantity=? where id=?";
-        template.update(sql,
-                updateParam.getItemName(),
-                updateParam.getPrice(),
-                updateParam.getQuantity(),
-                itemId);
+        String sql = "update item set item_name=:itemName, price=:price, quantity=:quantity where id=:id";
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("itemName", updateParam.getItemName())
+                .addValue("price", updateParam.getPrice())
+                .addValue("quantity", updateParam.getQuantity())
+                .addValue("id", itemId);
+        template.update(sql, param);
     }
 
     @Override
     public Optional<Item> findById(Long id) {
-        String sql = "select id, item_name, price, quantity from item where id = ?";
+        String sql = "select id, item_name, price, quantity from item where id = :id";
 
         try {
-            Item item = template.queryForObject(sql, itemRowMapper(), id);
+            Map<String, Object> param = Map.of("id", id);
+            Item item = template.queryForObject(sql, param, itemRowMapper());
             return Optional.of(item);
         } catch (EmptyResultDataAccessException e) {
             return Optional.empty();
@@ -79,6 +91,9 @@ public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
     public List<Item> findAll(ItemSearchCond cond) {
         String itemName = cond.getItemName();
         Integer maxPrice = cond.getMaxPrice();
+
+        SqlParameterSource param = new BeanPropertySqlParameterSource(cond);
+
         String sql = "select id, item_name, price, quantity from item";
 
         //동적 쿼리
@@ -86,32 +101,22 @@ public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
             sql += " where";
         }
         boolean andFlag = false;
-        List<Object> param = new ArrayList<>();
         if (StringUtils.hasText(itemName)) {
-            sql += " item_name like concat('%',?,'%')";
-            param.add(itemName);
+            sql += " item_name like concat('%',:itemName,'%')";
             andFlag = true;
         }
         if (maxPrice != null) {
             if (andFlag) {
                 sql += " and";
             }
-            sql += " price <= ?";
-            param.add(maxPrice);
+            sql += " price <= :maxPrice";
         }
         log.info("sql={}", sql);
-        return template.query(sql, itemRowMapper(), param.toArray());
+        return template.query(sql, param, itemRowMapper());
     }
 
     private RowMapper<Item> itemRowMapper() {
-        return ((rs, rowNum) -> {
-            Item item = new Item();
-            item.setId(rs.getLong("id"));
-            item.setItemName(rs.getString("item_name"));
-            item.setPrice(rs.getInt("price"));
-            item.setQuantity(rs.getInt("quantity"));
-            return item;
-        });
+        return BeanPropertyRowMapper.newInstance(Item.class); // camel 변환 지원
     }
 
 }
